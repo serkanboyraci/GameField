68a136ad0d1be0e35d33993e64726c0323f38828
serkanboyraci <serkanboyraci@gmail.com>
2023-01-19 16:34:07 +0300

    CollectionView added to HomeViewController

--- GameField/Feature/HomeModule/HomeViewController.swift
+++ GameField/Feature/HomeModule/HomeViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 protocol HomeViewControllerDelegate: AnyObject {
-    
-    
+    func prepareCollectionView()
+        
 }
 
+
 class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var gameCollectionView: UICollectionView!
     
     private lazy var viewModel: HomeViewModelDelegate = HomeViewModel()
 
@@ -22,6 +25,29 @@ class HomeViewController: UIViewController {
         viewModel.viewDidLoad() 
     }
 }
-extension HomeViewController: HomeViewControllerDelegate {
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel.numberOfItemsInSection()
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCollectionViewCell.identifier ,for: indexPath) as? HomeCollectionViewCell else {
+            print("xib not found")
+            return .init()
+        }
+        guard let game = viewModel.cellForItemAt(at: indexPath) else { return .init() }
+        
+        cell.configureCell(with: game)
+        return cell
+    }
 }
+
+
+extension HomeViewController: HomeViewControllerDelegate {
+    func prepareCollectionView() {
+        gameCollectionView.dataSource = self
+        gameCollectionView.register(HomeCollectionViewCell.nib, forCellWithReuseIdentifier: HomeCollectionViewCell.identifier)
+    }
+        
+}
+
