ac005d402c3066c4f1ef0b30d2ccda37fedca355
serkanboyraci <serkanboyraci@gmail.com>
2023-01-21 00:43:23 +0300

    FavoriteModule swift files added

---
+++ GameField/Feature/FavoriteListModule/FavoriteListCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteListCell.swift
+//  GameField
+//
+//  Created by Ali serkan Boyracı  on 21.01.2023.
+//
+
+import UIKit
+
+class FavoriteListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ GameField/Feature/FavoriteListModule/FavoriteListViewModule.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteListViewModule.swift
+//  GameField
+//
+//  Created by Ali serkan Boyracı  on 21.01.2023.
+//
+
+import Foundation
