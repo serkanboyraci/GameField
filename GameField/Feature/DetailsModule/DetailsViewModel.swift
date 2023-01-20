8ebbc69ecd0953bc9456aa27d0e103802195e6a0
serkanboyraci <serkanboyraci@gmail.com>
2023-01-20 15:43:38 +0300

    DetailsModule updated.

--- GameField/Feature/DetailsModule/DetailsViewModel.swift
+++ GameField/Feature/DetailsModule/DetailsViewModel.swift
@@ -9,17 +9,53 @@ import Foundation
 
 protocol DetailsViewModelDelegate {
     var view: DetailsViewControllerDelegate? { get set }
-    
-    func viewDidLoad(id: Int)
+    var id: Int! { get set }
+    func viewDidLoad()
+    func favoriteButtonClicked()
 }
-class DetailsViewModel: DetailsViewModelDelegate {
+
+final class DetailsViewModel: DetailsViewModelDelegate {
     //MARK: - Property
     private lazy var networkManager: NetworkManagerProtocol = NetworkManager()
     var view: DetailsViewControllerDelegate?
+    var id: Int!
+    var game: GameDetail?
+    var gameIsFavorite: Bool = false
+    
     
     //MARK: - Lifecycle
-    func viewDidLoad(id: Int) {
+    func viewDidLoad() {
         getGameDetails(by: id)
+        getGameIsFavorite()
+    }
+    
+    // Methods
+    func favoriteButtonClicked() {
+        guard let game else { return }
+        if !gameIsFavorite {
+            CoreDataFavoriteGameClient.shared.saveFavoriteGame(id: id.toString(), name: game.name){ [weak self] result in
+                switch result {
+                case .success(let success):
+                    self?.view?.showSuccessAlert(message: success.message)
+                case .failure(let error):
+                    self?.view?.showErrorAlert(message: error.message)
+                    return
+                }
+            }
+        } else {
+            CoreDataFavoriteGameClient.shared.deleteFavoriteGame(id: id.toString()){ [weak self] result in
+                switch result {
+                case .success(let success):
+                    self?.view?.showSuccessAlert(message: success.message)
+                case .failure(let error):
+                    self?.view?.showErrorAlert(message: error.message)
+                    return
+                }
+            }
+        }
+        gameIsFavorite.toggle()
+        view?.changeButtonColor(gameIsFavorite)
+        favoriteStatusDidChanged()
     }
     
     //MARK: - Private Methods
@@ -37,5 +73,23 @@ class DetailsViewModel: DetailsViewModelDelegate {
                 self?.game = game
             }
         }
+    }
+    
+    func getGameIsFavorite(){
+        guard let _ = CoreDataFavoriteGameClient.shared.getFavoriteGame(by: id.toString()) else {
+            self.gameIsFavorite = false
+            view?.changeButtonColor(false)
+            return
+        }
+        gameIsFavorite = true
+        view?.changeButtonColor(true)
+    }
+    
+    private func favoriteStatusDidChanged(){
+        CommunicationBetweenModules.shared.post(name: CommunicationMessage.changedFavoriteStatus.rawValue)
+    }
+    
+    private func sendNotificationForDataNotFound(){
+        CommunicationBetweenModules.shared.post(name: CommunicationMessage.favoriteGameDetailDataNotFound.rawValue)
     }
 }
